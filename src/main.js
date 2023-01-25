a7f04fd5a5d3f94c2381f734de9df11a75e613c6
kirkb <kirkbyjkk@gmail.com>
2023-01-25 18:38:00 +0000

    Added method to initialize scenes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+import {LoadScene} from "./scenes/LoadScene";
+import {MenuScene} from "./scenes/MenuScene";
+
 let game = new Phaser.Game({
     width: 100,
-    height: 100
+    height: 100,
+    scene:[
+        LoadScene, MenuScene
+    ]
 });
