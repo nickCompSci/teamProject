b9a918226eed3520e8f70454c21972124eeba5d3
kirkb <kirkbyjkk@gmail.com>
2023-01-25 22:25:21 +0000

    Imported OptionsScene

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import {LoadScene} from "./scenes/LoadScene";
 import {MenuScene} from "./scenes/MenuScene";
+import {OptionsScene} from "./scenes/OptionsScene"
 
 let game = new Phaser.Game({
     width: window.innerWidth,
     height: window.innerHeight,
     scene:[
-        LoadScene, MenuScene
+        LoadScene, MenuScene, OptionsScene
     ],
     render:{
         pixelArt: true
