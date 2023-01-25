c0a2cbf7ccf3b8ddd88f73d6ccebf63e63572cf1
kirkb <kirkbyjkk@gmail.com>
2023-01-25 21:07:29 +0000

    Added images to preload function

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -11,6 +11,12 @@ export class LoadScene extends Phaser.Scene{
     }
     preload(){
 
+    this.load.image("logo.png", "./assets/logo.png")
+    this.load.image("options.png", "./assets/options.png")
+    this.load.image("start.png", "./assets/start.png")
+    this.load.image("credits.png", "./assets/credits.png")
+    this.load.image("minigun-golden-3d-model-low-poly-obj-fbx-blend.jpg", "./assets/minigun-golden-3d-model-low-poly-obj-fbx-blend.jpg")
+
     }
     create(){
         this.scene.start(CST.SCENES.MENU, "Hello from load scene");
