f0bc9d44a0b57c3aaeef5e5c146d584136ff7c47
kirkb <kirkbyjkk@gmail.com>
2023-01-26 17:41:53 +0000

    Added account button image

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -14,10 +14,11 @@ export class LoadScene extends Phaser.Scene{
         this.load.image("logo", "./assets/logo.png");
         this.load.image("options", "./assets/options.png");
         this.load.image("start", "./assets/start.png");
+        this.load.image("back", "./assets/back.png")
+        this.load.image("account", "./assets/account.png")
         this.load.image("credits", "./assets/credits.png");
         this.load.image("background", "./assets/background.jpg");
         this.load.image("arrow", "./assets/arrow.png")
-        this.load.image("back", "./assets/back.png")
 
         this.load.audio("soundtrack", "./assets/soundtrack.mp3");
 
