685876fc4f2e4fc83d33fe4c4827ce159b9e7dae
kirkb <kirkbyjkk@gmail.com>
2023-01-26 10:22:34 +0000

    Added back button

--- src/scenes/OptionsScene.js
+++ src/scenes/OptionsScene.js
@@ -7,7 +7,29 @@ export class OptionsScene extends Phaser.Scene{
     }
     create(){
 
-        // Music, Controls, Back
+        // Music, Controls,
+        // Add custom fonts so don't have to add sprites everytime
         this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.20, "options").setDepth(1)
+
+        let backButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height /2 + 200, "back").setDepth(2)
+
+        let arrowSprite = this.add.sprite(100, 100, "arrow");
+        arrowSprite.setVisible(false);
+
+        // Credits Button
+        backButton.setInteractive();
+
+        backButton.on("pointerover", ()=>{
+            arrowSprite.setVisible(true);
+            arrowSprite.x = backButton.x - backButton.width +100;
+            arrowSprite.y = backButton.y;
+        })
+        backButton.on("pointerout", ()=>{
+            console.log("no hover")
+        })
+        backButton.on("pointerup", ()=>{
+            this.scene.start(CST.SCENES.MENU);
+            console.log("click")
+        })
     }
 }
