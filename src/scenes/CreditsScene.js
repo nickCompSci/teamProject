bee3b9ed889543e6916a909928a9e8269014c017
kirkb <kirkbyjkk@gmail.com>
2023-01-27 14:58:23 +0000

    Style: Changed title and buttons to use text + font, instead of an image

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -13,10 +13,10 @@ export class CreditsScene extends Phaser.Scene{
     create(){
 
         // Credits title
-        this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.20, "credits").setDepth(1)
+        this.add.text(this.game.renderer.width / 2, this.game.renderer.height * 0.20, 'Credits', {fontFamily: 'font1', fill: '#ffffff', fontSize: '60px'}).setDepth(1).setOrigin(0.5)
 
         // Back Button for navigating back to the main menu
-        let backButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height /2 + 300, "back").setDepth(2)
+        let backButton = this.add.text(this.game.renderer.width / 2, this.game.renderer.height / 2 + 300, 'Back', {fontFamily: 'font1', fill: '#ffffff', fontSize: '60px'}).setDepth(1).setOrigin(0.5)
 
         let arrowSprite = this.add.sprite(100, 100, "arrow");
         arrowSprite.setVisible(false);
