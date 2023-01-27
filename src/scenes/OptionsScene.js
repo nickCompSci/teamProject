20e6d9913e6f9b9f002481c29e290646d5bcc7f6
kirkb <kirkbyjkk@gmail.com>
2023-01-27 16:52:25 +0000

    Feat: Added disable music option which disables music when pressed

--- src/scenes/OptionsScene.js
+++ src/scenes/OptionsScene.js
@@ -15,6 +15,9 @@ export class OptionsScene extends Phaser.Scene{
         // Options title
         this.add.text(this.game.renderer.width / 2, this.game.renderer.height * 0.20, 'Options', {fontFamily: 'font1', fill: '#ffffff', fontSize: '60px'}).setDepth(1).setOrigin(0.5)
 
+        // Disable music button
+        let disableMusicButton = this.add.text(this.game.renderer.width / 2, this.game.renderer.height * 0.40, 'Disable Music', {fontFamily: 'font1', fill: '#ffffff', fontSize: '40px'}).setDepth(1).setOrigin(0.5)
+
         // Back Button for navigating back to the main menu
         let backButton = this.add.text(this.game.renderer.width / 2, this.game.renderer.height / 2 + 300, 'Back', {fontFamily: 'font1', fill: '#ffffff', fontSize: '60px'}).setDepth(1).setOrigin(0.5)
 
@@ -30,13 +33,37 @@ export class OptionsScene extends Phaser.Scene{
             arrowSprite.y = backButton.y + backButton.height / 4;
             console.log("hover")
         })
-        backButton.on("pointerout", ()=>{
-            console.log("no hover")
-        })
+
         backButton.on("pointerup", ()=>{
             // Moves back to the main menu when the back button is clicked
             this.scene.start(CST.SCENES.MENU);
             console.log("click")
+        })
+
+        // Disable Music Button
+        disableMusicButton.setInteractive();
+
+        disableMusicButton.on("pointerover", ()=>{
+            arrowSprite.setVisible(true);
+            arrowSprite.x = disableMusicButton.x - disableMusicButton.width + 120;
+            arrowSprite.y = disableMusicButton.y + disableMusicButton.height / 4;
+            console.log("hover")
+        })
+
+        // When clicked, music will be disabled
+        // When clicked again, music will be enabled
+        let musicEnabled = true;
+
+        disableMusicButton.on("pointerup", () => {
+            if (musicEnabled) {
+                this.sound.stopAll();
+                musicEnabled = false;
+                console.log("Music disabled");
+            } else {
+                this.sound.play("soundtrack", {loop: true});
+                musicEnabled = true;
+                console.log("Music enabled");
+            }
         })
     }
 }
