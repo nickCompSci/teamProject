c10223ea1dff4d178aff999dbc8053f5f3c93c7c
Nicholas <nickshapovcomp@gmail.com>
2023-02-16 23:36:29 +0000

    Feat: add login router to router

--- routes/main.js
+++ routes/main.js
@@ -16,3 +16,38 @@ router.post("/registration", passport.authenticate("registration",
     { session: false }), async (request, response, next) => {
         response.status(200).json({ message: "registration successful"});
 });
+
+router.post("/login", async (request, response, next) => {
+    passport.authenticate("login", async (err, user, info) => {
+        try {
+            if (err || !user) {
+                const error = new Error("An error occurred logging in.");
+                return next(error);
+            }
+            request.login(user, { session : false }, async (error) => {
+                if (error) return next(error);
+                const body = {
+                    _id: user._id,
+                    email: user.email
+                };
+                const token = jwt.sign({ user: body }, "top_secret", { expiresIn: 300});
+                const refreshToken = jwt.sign({ user: body }, "top_secret_refresh",
+                { expiresIn: 86400});
+                // store the jwt in a cookie
+                response.cookie("jwt", token);
+                response.cookie("refreshJwt", refreshToken);
+                //store the tokens in memory (tokenList)
+                tokenList[refreshToken] = {
+                    token,
+                    refreshToken,
+                    email: user.email,
+                    _id: user._id
+                };
+                // send the token back to the user
+                return response.status(200).json({ token, refreshToken});
+            });
+        } catch (error) {
+            return next(error);
+        }
+    })(request, response, next);
+});
