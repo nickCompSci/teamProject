db3dd47e8a071d5df3e1794598d15c0baae7cdd2
Nicholas <nickshapovcomp@gmail.com>
2023-02-16 21:08:31 +0000

    Feat: set up mongo connection in server app.js

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const mongoose = require("mongoose");
 // make instance of an express app
 const app = express();
 
+// setting up a mongo connection using mongoose
+const uri = process.env.MONGO_CONNECTION_URL;
+mongoose.connect(uri, {useNewUrlParser : true});
+// catch errors when connecting
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+    process.exit(1);
+});
+mongoose.connection.on('connected', function () {
+    console.log("Successfully connected to team4 mongo server");
+});
+
