08c4c6eb03849236de3ec440370b12e276b8d7c2
Nicholas <nickshapovcomp@gmail.com>
2023-02-16 23:12:43 +0000

    Feat: add logic for verifying a jwt token

--- auth/auth.js
+++ auth/auth.js
@@ -14,7 +14,7 @@ passport.use("registration", new localStrategy({
 }, async (request, email, password, done) => {
     try {
         const { username } = request.body;
-        const user = await UserModel.create({ email, password, username});
+        const user = await UserModel.create({ email, password, username });
         return done(null, user);
     } catch (error) {
         done(error);
@@ -30,14 +30,36 @@ passport.use("login", new localStrategy({
     try {
         const user = await UserModel.findOne({ email });
         if (!user) {
-            return done(null, false, { message: "Wrong email or password"});
+            return done(null, false, { message: "Wrong email or password" });
         }
         const validate = await user.isValidPassword(password);
         if (!validate) {
             return done(null, false, { message: "Wrong email or password" });
         }
-        return done(null, user, { message : "Logged in Successfully" });
+        return done(null, user, { message: "Logged in Successfully" });
     } catch (error) {
         return done(error);
+    }
+}));
+
+// verify jwt token of a user is valid
+passport.use(new JWTstrategy({
+    // used to sign the jwt that is created
+    // placeholder can be changed or env variable
+    secretOrKey: 'top_secret',
+    // function used to obtain the jwt from request object
+    // jwt will be inside a cookie, so must extract
+    jwtFromRequest: function (request) {
+        let token = null;
+        if (request && request.cookies) token = request.cookies["jwt"];
+        return token;
+    }
+    // callback function, call done function that was provided
+    // to the callback function
+}, async (token, done) => {
+    try {
+        return done(null, token.user);
+    } catch (error) {
+        done(error);
     }
 }));
