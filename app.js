98aac330d5995caaeedb4174f970266c33567e42
Nicholas <nickshapovcomp@gmail.com>
2023-02-16 21:22:33 +0000

    Feat: update express settings to utilize resources

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const passport = require('passport');
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-// make instance of an express app
-const app = express();
+const routes = require("./routes/main");
 
 // setting up a mongo connection using mongoose
 const uri = process.env.MONGO_CONNECTION_URL;
@@ -19,4 +17,25 @@ mongoose.connection.on('error', (error) => {
 mongoose.connection.on('connected', function () {
     console.log("Successfully connected to team4 mongo server");
 });
+
+// make instance of an express app
+const app = express();
+
+// updating express settings
+// parse app as x-www-form-urlencoded of bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse app/json
+app.use(bodyParser.json());
+// allow app to parse web cookies
+app.use(cookieParser());
+// the JS file used for user/profile authentication 
+require("./auth/auth");
+// where express will display/use static files
+app.use(express.static(__dirname + '/src'));
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+// the routes that are available, in routes dir
+app.use("/", routes);
+
 
