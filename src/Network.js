31f084b361243e91909c5d8fd3d5b9285326a1cb
jackom <120406576@umail.ucc.ie>
2023-02-19 21:48:41 +0000

    Docs: Add jsdoc comments

--- src/Network.js
+++ src/Network.js
@@ -1,12 +1,20 @@
+/**
+ * Network object represents network connection between two players,
+ * allowing for connection of players, sending and receiving data.
+ */
 class Network{
     constructor(){
         this.peer = new Peer();
         this._addPeerListeners();
     }
 
+    /**
+     * Add event listeners for peer events, handling instantiation of peer,
+     * connection by remote peer, and errors.
+     */
     _addPeerListeners(){
         this.peer.on('open', function(id){
-            console.log("Connected with id: ", id);
+            console.log("ID: ", id);
             this._id = id;    
         });
 
@@ -21,13 +29,22 @@ class Network{
         this.peer.on('error', function(err){
             console.log(err);
         });
+        return 
     }
 
+    /**
+     * Send data to the connected peer.
+     * @param {string} data The data to be sent.
+     */
     send(data){
         this.peer.conn.send(data);
         console.log("Data sent: ", data);
     }
 
+    /**
+     * Connect to remote peer using PeerJS. The data connection is saved.
+     * @param {*} id The peer id of the remote peer.
+     */
     connect(id){
         var conn;
         conn = this.peer.connect(id);
