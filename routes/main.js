68f633fceaf7c146196da38653c30b53e2f2e81e
Nicholas <nickshapovcomp@gmail.com>
2023-02-26 12:54:32 +0000

    Feat: add server route to delete a join code relationship

--- routes/main.js
+++ routes/main.js
@@ -435,6 +435,48 @@ router.post("/joinCodeRelationship", (request, response) => {
 
 })
 
+router.post("/deleteJoinRelationship", (request, response) => {
+    function deleteJoinRelationshipCallback(result) {
+        // at the moment wont return nothing
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({}));
+    }
 
+    function deleteJoinRelationship(currentUsername, joinCode, callback) {
+        (async () => {
+            // need to create the JOIN_CODE relationship to all friends of the current user
+            // can make it unique hence overwrite previous ones?
+            // main concern is what if a user backs out and reloads the scene 3 times?
+            // above should be covered by the below statement
+            // what happens to the relationship when a user backs out of the lobby scene??
 
+            // connection details
+            const uri = process.env.NEO4J_URI;
+            const user = process.env.NEO4J_USERNAME;
+            const password = process.env.NEO4J_PASSWORD;
+
+            const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+            try {
+                const session = driver.session({ database: "neo4j" });
+                // query create unique JOIN_CODE relationship
+                const writeQuery = `
+                    MATCH (a:Person {username: $currentUsername})-[r:JOIN_CODE]->(n:Person)
+                    DELETE r`;
+                await session.executeWrite(tx =>
+                    tx.run(writeQuery, { currentUsername, joinCode })
+                );
+            } catch (error) {
+                console.error(`Something went wrong: ${error}`);
+            } finally {
+                await driver.close();
+                callback();
+            }
+        })();
+    }
+    const refreshToken = request.body.refreshToken;
+    const currentUser = tokenList[refreshToken].username;
+    const joinCode = request.body.joinCode
+    deleteJoinRelationship(currentUser, joinCode,
+        deleteJoinRelationshipCallback);
+})
 module.exports = router;
